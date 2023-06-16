73263e071fcb071d934fa230c19368f8d85a64ed
V-sukumar <thalasuku2001@gmail.com>
2023-06-16 12:53:11 +0530

    day3

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import MainRoutes from "./Components/MainRoutes"
+import './App.css';
+import Navbar from "./Components/Navbar"
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <MainRoutes/>
+    </div>
+  );
+}
+
+export default App;
